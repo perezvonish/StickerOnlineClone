dfaa29e6d83a9e128a961155d5b6e2b34dd1f50b
Vladimir Ivanyta <shokilak@gmail.com>
2021-10-30 04:36:33 +0300

    init
    ended

---
+++ js/faqScript.js
@@ -0,0 +1,50 @@
+let questionOne = document.getElementById('faq1');
+let questionTwo = document.getElementById('faq2');
+let questionThrее = document.getElementById('faq3');
+let questionFour = document.getElementById('faq4');
+let questionFive = document.getElementById('faq5');
+let questionSix = document.getElementById('faq6');
+
+
+
+questionOne.onclick = addTextAnswer = () => {
+    let answer = document.getElementById('ans1');
+    if (answer.hidden){
+        answer.hidden = false;
+    } else answer.hidden = true;
+};
+
+questionTwo.onclick = addTextAnswer = () => {
+    let answer = document.getElementById('ans2');
+    if (answer.hidden){
+        answer.hidden = false;
+    } else answer.hidden = true;
+};
+
+questionThrее.onclick = addTextAnswer = () => {
+    let answer = document.getElementById('ans3');
+    if (answer.hidden){
+        answer.hidden = false;
+    } else answer.hidden = true;
+};
+
+questionFour.onclick = addTextAnswer = () => {
+    let answer = document.getElementById('ans4');
+    if (answer.hidden){
+        answer.hidden = false;
+    } else answer.hidden = true;
+};
+
+questionFive.onclick = addTextAnswer = () => {
+    let answer = document.getElementById('ans5');
+    if (answer.hidden){
+        answer.hidden = false;
+    } else answer.hidden = true;
+};
+
+questionSix.onclick = addTextAnswer = () => {
+    let answer = document.getElementById('ans6');
+    if (answer.hidden){
+        answer.hidden = false;
+    } else answer.hidden = true;
+};
